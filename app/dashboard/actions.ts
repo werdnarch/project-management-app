d7c14c2626f9daf094ef590f469365b7f8c8748b
werdnarch <andrewmuyeghu@gmail.com>
2025-08-26 12:19:49 +0200

    minor change

--- app/dashboard/actions.ts
+++ app/dashboard/actions.ts
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import { WelcomeMessage } from "../types";
+import { WelcomeMessage } from "@/types";
 
 export const getWelcomeMessage = async () => {
   const res = await fetch("/api/welcome");
